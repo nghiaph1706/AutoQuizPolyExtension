6b41b7c997fecf885525aeff0a563dedad76a99d
nghia <nghiaplps17855@fpt.edu.vn>
2022-01-06 20:12:25 +0700

    fix 162022
    fix quiz lms close

--- AutoQuizPoly/quizlms.js
+++ AutoQuizPoly/quizlms.js
@@ -1,30 +1,13 @@
+var refId, locate, quesCont, sequence, point;
 
-var refId, locate, pass, quesCont, sequence, activeId, count;
-var rsList = new Array();
-
-function getInfo() {
+async function getInfo() {
     var urlCurrent = window.location.href;
     refId = urlCurrent.substr(urlCurrent.lastIndexOf('ref_id='), 13).replace('ref_id=', '');
     locate = urlCurrent.substring(urlCurrent.lastIndexOf('http://') + 7, urlCurrent.lastIndexOf('.poly.edu.vn'));
-
     var temp = document.getElementsByClassName("small text-muted");
     quesCont = Number(temp[0].textContent.substring(temp[0].textContent.lastIndexOf('of') + 2, temp[0].textContent.lastIndexOf(`(`)).trim());
-    for (let i = 0; i < rsList.length; i++) {
-        rsList.push(0);
-    }
     sequence = Number(urlCurrent.substring(urlCurrent.lastIndexOf('sequence=') + 9, urlCurrent.indexOf('&active_id')));
-
-    activeId = urlCurrent.substring(urlCurrent.lastIndexOf('&active_id=') + 11, urlCurrent.lastIndexOf('&cmd=showQuestion&'), -1);
-}
-
-function sleep(time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
-}
-
-
-async function getResult() {
-    rsList.length = 0;
-    var url = 'http://' + locate + '.poly.edu.vn/ilias.php?ref_id=' + refId + '&active_id=&pass=' + pass + '&cmd=outUserPassDetails&cmdClass=iltestevaluationgui&cmdNode=q4:ll:vx&baseClass=ilRepositoryGUI';
+    var url = `http://` + locate + `.poly.edu.vn/ilias.php?ref_id=` + refId + `&cmd=outUserResultsOverview&cmdClass=iltestevaluationgui&cmdNode=q4:ll:vx&baseClass=ilrepositorygui`;
     await fetch(url)
         .then(function (response) {
             return response.text()
@@ -32,117 +15,115 @@ async function getResult() {
         .then(function (html) {
             var parser = new DOMParser();
             var doc = parser.parseFromString(html, "text/html");
-            var table = doc.querySelector('table');
-            for (var r = 0, n = table.rows.length; r < n; r++) {
-                rsList.push(Number(table.rows[r].cells[4].innerHTML));
-            }
+            var table = doc.querySelector('#tst_pass_overview_314685');
+            var tmpString = table.rows[table.rows.length - 1].cells[4].innerHTML;
+            point = Number(tmpString.substring(0, tmpString.lastIndexOf(`Of`)).trim());
         })
+    // console.log(`refId: ` + refId + ` locate: ` + locate + ` quesCont: ` + quesCont + ` sequence: ` + sequence + ` point: ` + point)
 }
 
+async function start() {
+    await getInfo();
+    var count = Number(localStorage.getItem('count'));
+    var prev = Number(localStorage.getItem('prev'));
+    var pointOld = Number(localStorage.getItem('pointOld'));
+    var ans = Number(localStorage.getItem('ans'));
+    // console.log(`count: ` + count + ` prev: ` + prev + ` pointOld: ` + pointOld + ` ans: ` + ans)
+    if (Number(null) == count) {
 
-async function start(ans) {
-    await getResult();
-    var rschild = rsList[sequence];
-    if (Number(rschild) == 0) {
+        localStorage.setItem('count', '1');
+        localStorage.setItem('ans', '1');
+        localStorage.setItem('pointOld', '0');
+        localStorage.setItem('prev', '1');
+
+        document.getElementById(`answer_0`).click();
+        var next = document.getElementById('bottomnextbutton');
+        next.click();
+
+        try {
+            var save = document.getElementById('save_on_navigation_prevent_confirmation');
+            save.click();
+            var confirm = document.getElementById('tst_save_on_navigation_button');
+            confirm.click();
+        } catch (error) {
+        }
+
+    } else if (prev == 1) {
+
+        localStorage.setItem('prev', '0');
+
+        var prev = document.getElementById('bottomprevbutton');
+        prev.click();
+
+    } else if (prev == 2) {
+        if (point == quesCont) {
+
+            localStorage.setItem('count', '' + Number(1000));
+            
+            var url = `http://` + locate + `.poly.edu.vn/ilias.php?ref_id=` + refId + `&cmd=outUserResultsOverview&cmdClass=iltestevaluationgui&cmdNode=q4:ll:vx&baseClass=ilrepositorygui`;
+            var popup = window.open(url, 'popUpWindow', 'height=700,width=700,left=900,top=50,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes')
+            
+        } else {
+            localStorage.setItem('prev', '0');
+            var next = document.getElementById('bottomnextbutton');
+            next.click();
+
+            try {
+                var save = document.getElementById('save_on_navigation_prevent_confirmation');
+                save.click();
+                var confirm = document.getElementById('tst_save_on_navigation_button');
+                confirm.click();
+            } catch (error) {
+            }
+
+        }
+    } else if (count < quesCont) {
+
+        if (point == sequence) {
+
+            localStorage.setItem('prev', '0');
+            localStorage.setItem('count', '' + Number(count + 1));
+            localStorage.setItem('ans', '' + Number(0));
+
+            var next = document.getElementById('bottomnextbutton');
+            next.click();
+
+            try {
+                var save = document.getElementById('save_on_navigation_prevent_confirmation');
+                save.click();
+                var confirm = document.getElementById('tst_save_on_navigation_button');
+                confirm.click();
+            } catch (error) {
+            }
+
+        } else {
+
+            localStorage.setItem('prev', '1');
+            localStorage.setItem('ans', '' + Number(ans + 1));
+            localStorage.setItem('pointOld', '' + Number(point));
+
+            document.getElementById(`answer_` + ans).click();
+            var next = document.getElementById('bottomnextbutton');
+            next.click();
+
+            try {
+                var save = document.getElementById('save_on_navigation_prevent_confirmation');
+                save.click();
+                var confirm = document.getElementById('tst_save_on_navigation_button');
+                confirm.click();
+            } catch (error) {
+            }
+
+        }
+
+    } else if (count == quesCont) {
+        localStorage.setItem('prev', '2');
+        localStorage.setItem('ans', '' + Number(ans + 1));
+
         document.getElementById(`answer_` + ans).click();
-    }
-    if (count == 0 || count == 2 || count == 99) {
-        try {
-            var next = document.getElementById('bottomnextbutton');
-            next.click();
-        } catch (error) {
-            var prev = document.getElementById('bottomprevbutton');
-            prev.click();
-        }
-    } else if (count == 1 || count == 3) {
-        try {
-            var prev = document.getElementById('bottomprevbutton');
-            prev.click();
-        } catch (error) {
-            var next = document.getElementById('bottomnextbutton');
-            next.click();
-        }
-    }
-    try {
-        var save = document.getElementById('save_on_navigation_prevent_confirmation');
-        save.click();
-        var confirm = document.getElementById('tst_save_on_navigation_button');
-        confirm.click();
-    } catch (error) {
-    }
-    localStorage.setItem('status', '' + (Number(tmpCount) + 1))
-
-
-}
-
-async function getPass(refId, locate) {
-    var url = 'http://' + locate + '.poly.edu.vn/ilias.php?ref_id=' + refId + '&cmd=outUserResultsOverview&cmdClass=iltestevaluationgui&cmdNode=q4:ll:vx&baseClass=ilrepositorygui';
-    await fetch(url)
-        .then(res => res.text())
-        .then(text => {
-            var tmp = text.split(`This object is empty and contains no items.`).length - 1;
-            if (tmp == 1) {
-                pass = 0;
-            } else {
-                pass = text.split(`<tr class="tblrow`).length - 1;
-                localStorage.setItem('pass', ''+pass);
-            }
-        })
-    start(count)
-}
-
-var tmpCount = Number(localStorage.getItem('status'))
-
-if (Number(null) == tmpCount) {
-    localStorage.setItem('status', '1');
-    tmpCount = Number(localStorage.getItem('status'))
-    count = 0;
-    getInfo()
-    getPass(refId, locate);
-} else if (Number(null) != tmpCount) {
-    tmpCount = Number(localStorage.getItem('status'));
-    getInfo();
-    if (tmpCount <= quesCont) { //1-44
-        count = 0;
-        getInfo();
-        getPass(refId, locate);
-    } else if (tmpCount > quesCont && tmpCount <= quesCont * 2) { //45-89
-        count = 1;
-        getInfo()
-        getPass(refId, locate);
-        if (tmpCount == quesCont*2) {
-            count = 2;
-            getInfo()
-            getPass(refId, locate);
-        }
-    } else if (tmpCount > quesCont * 2 && tmpCount <= quesCont * 3) { //90-134
-        count = 2;
-        getInfo()
-        getPass(refId, locate);
-        if (tmpCount == quesCont*3) {
-            count = 3;
-            getInfo()
-            getPass(refId, locate);
-        }
-    } else if (tmpCount > quesCont * 3 && tmpCount <= quesCont * 4) { //135-179
-        count = 3;
-        getInfo()
-        getPass(refId, locate);
-        if (tmpCount == quesCont*4) {
-            count = 3;
-            getInfo()
-            getPass(refId, locate);
-        }
-    } else {
-        localStorage.removeItem('status');
-        alert('done');
-        count = 999;
-        getInfo();
-        getPass(refId, locate);
-        var passnow = Number(localStorage.getItem('pass'))
-        var urlsnow = 'http://' + locate + '.poly.edu.vn/ilias.php?ref_id=' + refId + '&active_id=&pass=' + passnow + '&cmd=outUserPassDetails&cmdClass=iltestevaluationgui&cmdNode=q4:ll:vx&baseClass=ilRepositoryGUI';
-        var popup = window.open(urlsnow, 'popUpWindow', 'height=700,width=700,left=900,top=50,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes')
+        var prev = document.getElementById('bottomprevbutton');
+        prev.click();
     }
 }
 
-
+start();
--- AutoQuizPoly/remove.js
+++ AutoQuizPoly/remove.js
@@ -1,1 +1,4 @@
-localStorage.removeItem('status');
+localStorage.removeItem('ans');
+localStorage.removeItem('count');
+localStorage.removeItem('pointOld');
+localStorage.removeItem('prev');
